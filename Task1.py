b342d51fdf114e6d5b0224ba3cc2ffb1d82bbc51
Avaneesh1206 <53928506+Avaneesh1206@users.noreply.github.com>
2023-04-01 13:22:58 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -2,24 +2,31 @@
 # coding: utf-8
 
 # In[1]:
-
+Task 1:
 
 a=int(input('Enter number'))
 b=int(input('Enter number'))
 print(a+b)
+Enter number1
+Enter number2
+3
 
 
 # In[2]:
 
-
+Task 2:
 a=input('ENter String:')
 b=input('Enter String:')
 print('Combined String:',a+b)
+O/P:
+ENter String:avane
+Enter String:eesh
+Combined String: avaneeesh
 
 
 # In[3]:
 
-
+Task 3:
 File = open("task.txt",'w')
 print(File)  #To check if the file exists or not. IF the file dosen't exist it will throw a error
 File.write("Docker \n")
@@ -28,26 +35,35 @@ File.write("Linux")
 File.close()
 
 File = open("task.txt", "r+")
-
+O/P:<_io.TextIOWrapper name='task.txt' mode='w' encoding='cp1252'>
 
 # In[4]:
 
-
+Task 4:
 a=input('Enter string:')
 for i in range(len(a)-1,-1,-1):
     print(a[i])
-
+O/P:
+Enter string:abcd
+d
+c
+b
+a
 
 # In[5]:
 
-
+Task 5:
 a=input('Enter 10 characters:')
 for i in range(len(a)):
     if i==2:
         print(a[i])
     if i==7:
         print(a[i])
-    
+ 
+O/P:
+Enter 10 characters:abcdefghij
+c
+h
 
 
 # In[ ]:
